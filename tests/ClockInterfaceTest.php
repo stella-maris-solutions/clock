c04f22cc98e44c441c516416ac83ab5513d8f5ba
Andreas Heigl <andreas@heigl.org>
2022-04-17 16:28:39 +0200

    Add tests

---
+++ tests/ClockInterfaceTest.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Copyright Andreas Heigl <andreas@heigl.org>
+ *
+ * Licenses under the MIT-license. For details see the included file LICENSE.md
+ */
+
+namespace StellaMaris\ClockTest;
+
+use PHPUnit\Framework\TestCase;
+use StellaMaris\Clock\ClockInterface;
+
+class ClockInterfaceTest extends TestCase
+{
+    /**
+     * @testdox The interface can be autoloaded
+     * @covers ClockInterface::now;
+     */
+    public function testInterfaceIsUsable(): void
+    {
+        self::assertInstanceof(ClockInterface::class, $this->getMockBuilder(ClockInterface::class)->getMock());
+    }
+}
