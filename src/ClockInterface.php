fa23ce16019289a18bb3446fdecd45befcdd94f8
Andreas Heigl <andreas@heigl.org>
2022-11-25 17:15:06 +0100

    Extend the just released PSR-20 interface

--- src/ClockInterface.php
+++ src/ClockInterface.php
@@ -8,11 +8,8 @@
 namespace StellaMaris\Clock;
 
 use DateTimeImmutable;
+use Psr\Clock\ClockInterface as PsrClockInterface;
 
-interface ClockInterface
+interface ClockInterface extends PsrClockInterface
 {
-	/**
-	 * Return the current point in time as a DateTimeImmutable object
-	 */
-	public function now() : DateTimeImmutable;
 }
